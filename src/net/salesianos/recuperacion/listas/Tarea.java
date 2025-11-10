f516c88064955aee68ae3afda6485f6c3d30d29a
Gabriel <usr4035@salesianos-lacuesta.net>
2025-11-10 17:41:35 +0000

    feat:Se creo la clase tarea juntos sus atributos y constructores

---
+++ src/net/salesianos/recuperacion/listas/Tarea.java
@@ -0,0 +1,34 @@
+package src.net.salesianos.recuperacion.listas;
+import java.time.LocalDate;
+
+public class Tarea {
+    private String titulo;
+    private String descripcion;
+    private String prioridad;
+    private LocalDate fecha;
+
+    public Tarea(String titulo, String descripcion, String prioridad, LocalDate fecha) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.fecha = fecha;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public String getPrioridad() {
+        return prioridad;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    
+    @Override
+    public String toString() {
+        return "Tarea [titulo=" + titulo + ", descripcion=" + descripcion + ", prioridad=" + prioridad + ", fecha="
+                + fecha + "]";
+    }
+}
